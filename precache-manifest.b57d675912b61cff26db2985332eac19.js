307c7951b4f6e6c8493cefe7243ba0736e4023c2
huy-xu <nhathuyhedspi@gmail.com>
2020-02-18 18:23:29 +0700

    Updates

---
+++ precache-manifest.b57d675912b61cff26db2985332eac19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf173afd8924f9bd4db629426a4ab39",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "ef88005c9476021291c6",
+    "url": "/todo-app/static/css/main.a66306f0.chunk.css"
+  },
+  {
+    "revision": "da4b94b093b671e9d45b",
+    "url": "/todo-app/static/js/2.7584bdca.chunk.js"
+  },
+  {
+    "revision": "fcf7bb13763b5d6e269b43e96defdfd0",
+    "url": "/todo-app/static/js/2.7584bdca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef88005c9476021291c6",
+    "url": "/todo-app/static/js/main.0075826a.chunk.js"
+  },
+  {
+    "revision": "b66ecab6fc712c6d22a7",
+    "url": "/todo-app/static/js/runtime-main.e77e13dd.js"
+  },
+  {
+    "revision": "d7c94a31b868bae4a15510057601e91a",
+    "url": "/todo-app/static/media/Todo.d7c94a31.jpg"
+  }
+]);
