8505854661d385a9bd65ac92512379a56ee05132
huy-xu <nhathuyhedspi@gmail.com>
2020-04-02 10:15:25 +0700

    Responsive Landing Page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import './LandingPage.css';
-import { Link } from 'react-router-dom';
-import * as ROUTES from '../../constants/routes';
+import React, { Component } from "react";
+import "./LandingPage.css";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
 
 class LandingPage extends Component {
   render() {
@@ -9,11 +9,15 @@ class LandingPage extends Component {
       <div className="LandingPage text-center">
         <div className="bg-image"></div>
         <div className="bg-text p-5">
-          <h1 className="display-1 text-light">Create your TODO list</h1>
+          <h1 className="display-1 text-light" style={{ fontSize: "10vw" }}>
+            Create your TODO list
+          </h1>
           <Link to={ROUTES.SIGN_IN}>
-            <button type="button" className="btn btn-outline-light btn-lg">Get start</button>
+            <button type="button" className="btn btn-outline-light btn-lg">
+              Get start
+            </button>
           </Link>
-        </div>       
+        </div>
       </div>
     );
   }
