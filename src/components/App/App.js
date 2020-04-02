f03e4c16e7040e96b0dde4ac7fd47a9c27abde76
huy-xu <nhathuyhedspi@gmail.com>
2020-04-02 10:08:14 +0700

    Responsive Navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,131 +1,131 @@
-import React, { Component } from 'react';
-import './App.css';
-import Particles from 'react-particles-js';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import * as ROUTES from '../../constants/routes';
+import React, { Component } from "react";
+import "./App.css";
+import Particles from "react-particles-js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
 
-import Navigation from '../Navigation/Navigation';
-import Notify from '../Notify/Notify';
-import LandingPage from '../LandingPage/LandingPage';
-import HomePage from '../HomePage/HomePage';
-import SignInPage from '../SignInPage/SignInPage';
-import SignUpPage from '../SignUpPage/SignUpPage';
+import Navigation from "../Navigation/Navigation";
+import Notify from "../Notify/Notify";
+import LandingPage from "../LandingPage/LandingPage";
+import HomePage from "../HomePage/HomePage";
+import SignInPage from "../SignInPage/SignInPage";
+import SignUpPage from "../SignUpPage/SignUpPage";
 
 const particlesOptions = {
-  "particles": {
-    "number": {
-      "value": 180,
-      "density": {
-        "enable": true,
-        "value_area": 868.1035095057337
+  particles: {
+    number: {
+      value: 180,
+      density: {
+        enable: true,
+        value_area: 868.1035095057337
       }
     },
-    "color": {
-      "value": "#ffffff"
+    color: {
+      value: "#ffffff"
     },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000"
       },
-      "polygon": {
-        "nb_sides": 4
+      polygon: {
+        nb_sides: 4
       },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100
       }
     },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
       }
     },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false
       }
     },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
     },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
       }
     }
   },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
       },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
+      onclick: {
+        enable: true,
+        mode: "push"
       },
-      "resize": true
+      resize: true
     },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1
         }
       },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3
       },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
+      repulse: {
+        distance: 200,
+        duration: 0.4
       },
-      "push": {
-        "particles_nb": 4
+      push: {
+        particles_nb: 4
       },
-      "remove": {
-        "particles_nb": 2
+      remove: {
+        particles_nb: 2
       }
     }
   },
-  "retina_detect": true
-}
+  retina_detect: true
+};
 
 class App extends Component {
   render() {
     return (
-      <Router>        
+      <Router>
         <div className="App">
           <Particles className="particles" params={particlesOptions} />
           <Navigation />
@@ -135,7 +135,7 @@ class App extends Component {
             <Route path={ROUTES.HOME} component={HomePage} />
             <Route path={ROUTES.SIGN_IN} component={SignInPage} />
             <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-          </Switch>          
+          </Switch>
         </div>
       </Router>
     );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,45 +1,70 @@
-import React, { Component, Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import * as ROUTES from '../../constants/routes';
+import React, { Component, Fragment } from "react";
+import "./Navigation.css";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
 
-import { connect } from 'react-redux';
-import { signOut } from '../../redux/actions/auth';
+import { connect } from "react-redux";
+import { signOut } from "../../redux/actions/auth";
 
 export class Navigation extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-sm navbar-light bg-light">
-        <Link to={ROUTES.HOME} className="navbar-brand">TODO</Link>
-        <div className="collapse navbar-collapse justify-content-end" id="collapsibleNavId">
-          <ul className="navbar-nav mt-2 mt-lg-0">
-            {this.props.authenticated
-              ? <li className="nav-item active" onClick={this.props.signOut}>
-                  <Link to={ROUTES.LANDING} className="nav-link">Sign out</Link>
-                </li>
-              : <Fragment>
+        <Link to={ROUTES.HOME} className="navbar-brand">
+          TODO
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapsibleNavId"
+          aria-controls="collapsibleNavId"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="collapsibleNavId"
+        >
+          <ul className="nav navbar-nav mt-2 mt-lg-0">
+            {this.props.authenticated ? (
+              <li className="nav-item active" onClick={this.props.signOut}>
+                <Link to={ROUTES.LANDING} className="nav-link">
+                  Sign out
+                </Link>
+              </li>
+            ) : (
+              <Fragment>
                 <li className="nav-item active">
-                  <Link to={ROUTES.SIGN_IN} className="nav-link">Sign in</Link>
+                  <Link to={ROUTES.SIGN_IN} className="nav-link">
+                    Sign in
+                  </Link>
                 </li>
                 <li className="nav-item active">
-                  <Link to={ROUTES.SIGN_UP} className="nav-link">Sign up</Link>
+                  <Link to={ROUTES.SIGN_UP} className="nav-link">
+                    Sign up
+                  </Link>
                 </li>
               </Fragment>
-            }
+            )}
           </ul>
         </div>
       </nav>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   authenticated: state.auth.authenticated
-})
+});
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     signOut: () => dispatch(signOut())
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Navigation)
+export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
